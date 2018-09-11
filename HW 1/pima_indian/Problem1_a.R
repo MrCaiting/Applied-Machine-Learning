46f1e78edaa35a66045d8ea461e2f674c942c4a6
Caiting Wu <mrctwu@gmail.com>
2018-09-11 01:28:17 -0500

    This is FINAL

--- HW 1/pima_indian/Problem1_a.R
+++ HW 1/pima_indian/Problem1_a.R
@@ -18,35 +18,33 @@ num.features <- ncol(dataset)-1
 label.name <- colnames(dataset)[ncol(dataset)]
 
 for (each in 1: trials){
-  
+
   trainIndex <- createDataPartition(dataset$V9, p = 0.8, list = FALSE)
   train <- dataset[ trainIndex,]
   test <- dataset[-trainIndex,]
   numTrain <- nrow(train)
   
-  # train.label0 <- train[which(train$V9 == 0),]
-  # train.label1 <- train[which(train$V9 == 1),]
   train.label0 <- as.matrix(train[which(train$V9 == 0),])[, 1:num.features]
   train.label1 <- as.matrix(train[which(train$V9 == 1),])[, 1:num.features]
-  
+
   # Getting priors
   train.label0.prior <- nrow(train.label0) / numTrain
   train.label1.prior <- nrow(train.label1) / numTrain
-  
+
   # Assuming all features across samples are represented as normal model
   # Getting mean and standard deviation
   train.label0.expect <- colMeans(train.label0)
   train.label1.expect <- colMeans(train.label1)
-  
+
   train.label0.std <- colSds(train.label0)
   train.label1.std <- colSds(train.label1)
-  
-  # Creating new column in 
+
+  # Creating new column in
   test$predict <- NA
-  
+
   # Making prediction
   for (idx in 1:nrow(test)){
-    
+
     x <- as.numeric(test[idx, 1:num.features])
     pred.zero <-sum(dnorm(x, mean = train.label0.expect, sd = train.label0.std, log = TRUE)) + log(train.label0.prior)
     pred.one <- sum(dnorm(x, mean = train.label1.expect, sd = train.label1.std, log = TRUE)) + log(train.label1.prior)
@@ -56,7 +54,7 @@ for (each in 1: trials){
       test[idx, ]$predict <- 0
     }
   }
-  
+
   acc_list[each] <- sum(test$V9 == test$predict) / nrow(test)
 }
 
--- HW 1/pima_indian/Problem1_d.R
+++ HW 1/pima_indian/Problem1_d.R
@@ -14,22 +14,21 @@ acc_list <- rep(0, trials)
 num.features <- ncol(dataset)-1
 
 for (each in 1: trials){
-  
+
   trainIndex <- createDataPartition(dataset$V9, p = 0.8, list = FALSE)
   train <- dataset[ trainIndex,]
   test <- dataset[-trainIndex,]
-  
+
   train.feature <- train[, -c(9)]
   train.label <- train[, 9]
   test.feature <- test[, -c(9)]
   test.label <- test[, 9]
   num_test <- nrow(test)
-  
+
   svm_model <- svmlight(train.feature, train.label, pathsvm = path)
-  
-  
+
   prediction <- predict(svm_model, test.feature)
-  
+
   acc_list[each] <- sum(test.label == prediction$class) / num_test
 }
 
